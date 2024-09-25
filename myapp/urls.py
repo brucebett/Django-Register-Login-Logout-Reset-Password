9b6f539a8cdae553638213a874de3d3e87a7feca
brucebett <bett6448@gmail.com>
2024-09-25 19:43:50 +0300

    wen commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.Home, name='home'),
+    path('register/', views.RegisterView, name='register'),
+    path('login/', views.LoginView, name='login'),
 ]
