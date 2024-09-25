536bec2ca743c08422514d8f13fe92b33ebadf7b
brucebett <bett6448@gmail.com>
2024-09-25 20:55:03 +0300

    wen1 commit

--- myapp/views.py
+++ myapp/views.py
@@ -12,19 +12,64 @@ from django.utils import timezone
 from django.urls import reverse
 from .models import *
 
+
+@login_required
 def Home(request):
     return render(request, 'index.html')
 
 def RegisterView(request):
 
     if request.method =='POST':
-        first_name = request.POST.get(first_name)
-        second_name = request.POST.get(second_name)
-        username = request.POST.get(username)
-        email = request.POST.get(email)
-        password = request.POST.get(password)
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
 
+        User_data_has_error = False
+
+        if User.objects.filter(username=username).exists():
+            User_data_has_error = True
+            messages.error(request, "Username already exist")
+
+        if User.objects.filter(email=email).exists():
+            User_data_has_error = True
+            messages.error(request, "Email already exist")   
+
+        if len(password) < 5:
+            User_data_has_error = True
+            messages.error(request, "Password must be atleast 5 characters")    
+
+        if User_data_has_error:
+            return redirect('register')   
+        else:
+            new_user = User.objects.create_user(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                username=username,
+                password=password
+            )
+            messages.success(request, 'Account create successfuly, login now')
+            return redirect('login')
+ 
     return render(request, 'register.html')
 
+
+
 def  LoginView(request):
+
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+            return redirect('home')
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect('login')
     return render(request, 'login.html')
